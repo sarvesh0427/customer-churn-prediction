1f364a702a49b902973e08d7213b264a36a72f2b
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-15 22:07:45 +0545

    updating preprcoessing

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
-def clean_data(df: pd.DataFrame):
 
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
 
-    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    # Convert TotalCharges to numeric
+    if "TotalCharges" in df.columns:
+        df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
 
+# remove missing values
     df.dropna(inplace=True)
+
+    # remove duplicate row
+    df.drop_duplicates(inplace=True)
+
+    # drop ID column 
+    if "customerID" in df.columns:
+        df.drop(columns=["customerID"], inplace=True)
+
+    df.reset_index(drop=True, inplace=True)
 
     return df
