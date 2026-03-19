002ea120c18af5ceb3fa24c7bba0692bc5bc3016
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-19 22:27:47 +0545

    updated prediction module

--- src/models/predict.py
+++ src/models/predict.py
@@ -4,12 +4,8 @@ import joblib
 def load_model(path):
 
     model = joblib.load(path)
-
     return model
 
-
 def predict(model, data):
-
     prediction = model.predict(data)
-
     return prediction
