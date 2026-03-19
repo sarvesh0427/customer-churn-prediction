cb318fdbc9e29085b3602b329485d9a9382acd50
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-19 22:29:25 +0545

    helper function updated

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,10 +1,31 @@
 import joblib
+from pathlib import Path
+from typing import Any
 
 
-def save_object(obj, filepath):
-    joblib.dump(obj, filepath)
+def save_object(obj: Any, filepath: str) -> None:
+    path = Path(filepath)
+
+    # create directory if it doesn't exist
+    path.parent.mkdir(parents=True, exist_ok=True)
+    try:
+        joblib.dump(obj, path)
+        print(f"Object saved successfully at {filepath}")
+
+    except Exception as e:
+        raise RuntimeError(f"Error saving object: {e}")
 
 
-def load_object(filepath):
+def load_object(filepath: str) -> Any:
+    path = Path(filepath)
 
-    return joblib.load(filepath)
+    if not path.exists():
+        raise FileNotFoundError(f"File not found at: {filepath}")
+
+    try:
+        obj = joblib.load(path)
+        print(f"Object loaded successfully from {filepath}")
+        return obj
+
+    except Exception as e:
+        raise RuntimeError(f"Error loading object: {e}")
