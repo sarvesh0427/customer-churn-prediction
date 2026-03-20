884060e5a1aaf9c40871cc4a81c503c77e2fb1e9
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-20 22:15:46 +0545

    updated code included in pipeline

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,8 +1,9 @@
 from src.data.load_data import load_data
 from src.data.preprocess import clean_data
 from src.features.build_features import create_features, split_features_target
-from src.models import train_model, evaluate
-import joblib
+from src.models.train_model import train_model
+from src.models.evaluate import evaluate_model
+from src.utils.helpers import save_object
 
 DATA_PATH = "data/clean_data.csv"
 MODEL_PATH = "models/churn_model.pkl"
@@ -11,29 +12,31 @@ FEATURE_PATH = "models/feature_columns.pkl"
 
 def run_training():
     print("Starting training pipeline...")
-    # 1.load data
+
+    # load data
     df = load_data(DATA_PATH)
 
-    # 2. Clean data
+    # Clean data
     df = clean_data(df)
-
-    # 3. feature engineering
+    # feature engineering
     df = create_features(df)
-
-    # 4. split features and target
+    # split features and target
     X, y = split_features_target(df)
-    # 5. Train model
+
+    print(f"Dataset ready: {X.shape[0]} rows, {X.shape[1]} features")
+
+    # Train model
     model, X_test, y_test = train_model(X, y)
 
-    # 6. Evaluate model
-    evaluate(model, X_test,y_test)
+    # evaluate model
+    evaluate_model(model, X_test, y_test)
+    # save model
+    save_object(model, MODEL_PATH)
 
-    # 7. Save model
-    # save_model(model,MODEL_PATH)
-    # 8. Save feature columns
-    joblib.dump(X.columns.tolist(), FEATURE_PATH)
-    print(f"Feature columns saved to {FEATURE_PATH}")
+    # Save feature columns (VERY IMPORTANT)
+    save_object(X.columns.tolist(), FEATURE_PATH)
     print("Training pipeline completed successfully!")
+
 
 if __name__ == "__main__":
     run_training()
