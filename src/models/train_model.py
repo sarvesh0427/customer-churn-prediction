93a9dae5a735cfc960f2812d9e1af59b0f2f545b
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-20 22:12:01 +0545

    parameters added and updated

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,25 +1,45 @@
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.pipeline import Pipeline
 
 
-def train_model(X, y):
+def train_model(X, y, preprocessor):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y,
         test_size=0.2,
         random_state=42,
         stratify=y
     )
-
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
-    )
+    model = Pipeline(steps=[
+        ("preprocessor", preprocessor),
+        ("classifier", RandomForestClassifier(
+            n_estimators=200,
+            max_depth=8,
+            min_samples_split=5,
+            class_weight="balanced",
+            random_state=42
+        ))
+    ])
 
     model.fit(X_train, y_train)
+
     return model, X_test, y_test
 
+# def train_model(X, y):
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         X, y,
+#         test_size=0.2,
+#         random_state=42,
+#     )
+
+#     model = RandomForestClassifier(
+#         n_estimators=100,
+#         random_state=42
+#     )
+
+#     model.fit(X_train, y_train)
+#     return model, X_test, y_test
 
 
 def save_model(model, path):
