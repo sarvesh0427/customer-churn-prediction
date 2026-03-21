ab108a3d6375609625963846aeb01ee018fe4d4d
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-21 21:45:58 +0545

    droping unnecessary column

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
-
-    # Only essential fixes
-
+    # fixing only required columm
     if "customerID" in df.columns:
         df.drop(columns=["customerID"], inplace=True)
     df.reset_index(drop=True, inplace=True)
