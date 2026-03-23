cc2b5cc260d0741dd580afd2366f829718798a5b
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-23 21:57:59 +0545

    taking best parameter from notebook

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -26,22 +26,6 @@ def train_model(X, y, preprocessor):
 
     return model, X_test, y_test
 
-# def train_model(X, y):
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         X, y,
-#         test_size=0.2,
-#         random_state=42,
-#     )
-
-#     model = RandomForestClassifier(
-#         n_estimators=100,
-#         random_state=42
-#     )
-
-#     model.fit(X_train, y_train)
-#     return model, X_test, y_test
-
-
 def save_model(model, path):
     joblib.dump(model, path)    # save trained model
     print(f"Model saved to {path}")
