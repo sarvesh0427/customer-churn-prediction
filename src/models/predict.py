8b88ad4349f6e47800c9eb7f5dc8ec57afbb9f8a
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-24 20:58:54 +0545

    prediction and probability both added

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,11 +1,27 @@
 import joblib
+import numpy as np
 
+def load_model(path: str):
+    try:
+        model = joblib.load(path)
+        print(f'Model loaded from {path}')
+        return model
+    except Exception as e:
+        raise RuntimeError(f"Error...Failed to load model: {e}")
 
-def load_model(path):
-
-    model = joblib.load(path)
-    return model
 
 def predict(model, data):
-    prediction = model.predict(data)
-    return prediction
+    try:
+        prediction = model.predict(data)
+        return prediction
+    except Exception as e:
+        raise RuntimeError(f"Error!!!Prediction failed: {e}")
+
+
+def predict_proba(model, data):
+    try:
+        proba = model.predict_proba(data)
+        return proba
+    
+    except Exception as e:
+        raise RuntimeError(f'Error!!!Probability prediction failed: {e}')
