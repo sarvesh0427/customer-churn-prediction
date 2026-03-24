33cfced2374fbb4ddc0b336970478ad461daa6e4
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-24 21:00:00 +0545

    roc-auc added

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,12 +1,24 @@
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 
 
 def evaluate_model(model, X_test, y_test):
 
     predictions = model.predict(X_test)
-
+    # accuracy
     acc = accuracy_score(y_test, predictions)
 
+    # classification report
     report = classification_report(y_test, predictions)
 
-    return acc, report
+    # ROC-AUC 
+    try:
+        proba = model.predict_proba(X_test)[:, 1]
+        roc_auc = roc_auc_score(y_test, proba)
+    except:
+        roc_auc = None
+
+    return {
+        "accuracy": acc,
+        "roc_auc": roc_auc,
+        "report": report
+    }
