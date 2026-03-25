185cf5f8f7b2a5ebc9696230088bd613ac2b33e5
sarvesh0427 <kshetrysarvesh@gmail.com>
2026-03-25 16:49:28 +0545

    preprocessor and model together at single pipeline

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,47 +1,50 @@
 from src.data.load_data import load_data
 from src.data.preprocess import clean_data
 from src.features.build_features import create_features, split_features_target
+from src.features.preprocessor import build_preprocessor
 from src.models.train_model import train_model
 from src.models.evaluate import evaluate_model
 from src.utils.helpers import save_object
-from src.features.preprocessor import build_preprocessor
+
 
 DATA_PATH = "data/clean_data.csv"
-MODEL_PATH = "models/churn_model.pkl"
-FEATURE_PATH = "models/feature_columns.pkl"
+MODEL_PATH = "models/final_pipeline.pkl"
 
-X = df.drop("Churn", axis=1)
-y = df["Churn"]
 
-numerical_features = X.select_dtypes(include=["int64", "float64"]).columns
-categorical_features = X.select_dtypes(include=["object"]).columns
-
-preprocessor = build_preprocessor(numerical_features, categorical_features)
 def run_training():
-    print("Starting training pipeline...")
+    print("...Starting training pipeline...")
 
-    # load data
+    # Load data
     df = load_data(DATA_PATH)
 
     # Clean data
     df = clean_data(df)
-    # feature engineering
+    # Feature engineering
     df = create_features(df)
-    # split features and target
-    X, y = split_features_target(df)
 
-    print(f"Dataset ready: {X.shape[0]} rows, {X.shape[1]} features")
+    # Splitting features and target 
+    X, y =split_features_target(df)
+
+    print(f'Dataset ready: {X.shape[0]} rows, {X.shape[1]} features')
+
+    # Build preprocessor
+    numerical_features = X.select_dtypes(include=["int64", "float64"]).columns
+    categorical_features = X.select_dtypes(include=["object"]).columns
+
+    preprocessor = build_preprocessor(numerical_features, categorical_features)
 
     # Train model
-    model, X_test, y_test = train_model(X, y)
+    model, X_test, y_test = train_model(X, y, preprocessor)
 
-    # evaluate model
-    evaluate_model(model, X_test, y_test)
-    # save model
+    # Evaluate model
+    metrics = evaluate_model(model, X_test, y_test)
+
+    print(f"Model Accuracy: {metrics['accuracy']}")
+    print(f"ROC-AUC: {metrics['roc_auc']}")
+    print("Classification Report:\n ", metrics["report"])
+
+    # save full pipeline
     save_object(model, MODEL_PATH)
-
-    # Save feature columns (VERY IMPORTANT)
-    save_object(X.columns.tolist(), FEATURE_PATH)
     print("Training pipeline completed successfully!")
 
 
